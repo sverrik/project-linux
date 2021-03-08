f35978c67897ddf1724dffd537a403f646fcd0bc
sverrik <80036098+sverrik@users.noreply.github.com>
2021-03-08 10:50:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def toggle_device(id):
             return dev.state
  
 if __name__ == '__main__':
-    app.run()(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
